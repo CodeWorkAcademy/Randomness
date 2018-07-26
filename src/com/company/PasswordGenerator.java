e9544e55f69a015b775ef0e02304cade6f0643e2
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-26 10:06:10 -0400

    Initial Commit

---
+++ src/com/company/PasswordGenerator.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Random;
+
+public class PasswordGenerator {
+    public static void main(String[] args)
+    {
+        output("Generate 10 random integers between 1 and 99");
+
+        Random rnd = new Random();
+
+        for (int i = 1; i <= 8; ++i)
+        {
+            int randomInt = 40 + rnd.nextInt(126);
+            char c= (char) randomInt;
+            output("Generated number: " +c);
+        }
+
+        output("Done.");
+    }
+
+    private static void output(String aMessage)
+    {
+        System.out.println(aMessage);
+    }
+}
